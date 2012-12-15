f285c76c745d51c53d4649b54cc67deb081210da
TING-KUANG WEI <twei@TING-KUANGs-MacBook-Pro.local>
2012-12-14 23:21:55 -0800

    Add more test cases for LessWeightLenDiff

--- HW_1/Q_1_2/test__Job.cpp
+++ HW_1/Q_1_2/test__Job.cpp
@@ -14,11 +14,17 @@ TEST(LWLR_1, LessWeightLenRatio)
 
 TEST(LWLD_1, LessWeightLenDiff)
 {
+    Job j0(4, 1);
     Job j1(61, 58);
     Job j2(86, 46);
+    Job j3(87, 46);
+    Job j4(87, 45);
 
     LessWeightLenDiff less_diff;
+    ASSERT_TRUE(less_diff(j0, j1));
     ASSERT_TRUE(less_diff(j1, j2));
+    ASSERT_TRUE(less_diff(j2, j3));
+    ASSERT_TRUE(less_diff(j3, j4));
 }
 
 int main(int argc, char *argv[])
