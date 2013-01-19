87e231b5354412e19b92c29d0638abbe9b447554
TING-KUANG WEI <twei@TING-KUANGs-MacBook-Pro.local>
2013-01-18 21:39:00 -0800

    basic prototype for John's algorithm

--- HW_4/all_pair_shortest_path.py
+++ HW_4/all_pair_shortest_path.py
@@ -2,6 +2,7 @@
 import sys
 from pprint import pprint
 
+# Bellman-Ford
 def initialize(edges, source):
     distance = {}
 
@@ -11,7 +12,6 @@ def initialize(edges, source):
     distance[source] = 0
 
     return distance
-
 
 def relax(u, v, edges, distance):
     if distance[v] > distance[u] + edges[u][v]:
@@ -28,10 +28,19 @@ def bellman_ford(edges, source):
     for u in edges:
         for v in edges[u]:
             assert (distance[v] <= distance[u] + edges[u][v]), "Negative Cycle"
-
     return distance
 
+# Johnson's
+def reweight_edges(edges, distance):
+    for u in edges.keys():
+        for v in edges[u]:
+            edges[u][v] = edges[u][v] + distance[u] - distance[v]
+    return edges
 
+def restore_edge_weight(positive_edge_weight, u, v, distance):
+    orig_edge_weight = positive_edge_weight - distance[u] + distance[v]
+    return orig_edge_weight
+     
 if __name__ == '__main__':
     graph_file = sys.argv[1] 
 
@@ -39,6 +48,8 @@ if __name__ == '__main__':
 
     with open(graph_file) as in_file:
         num_vertices, num_edges = in_file.readline().split(' ')
+        num_vertices = int(num_vertices)
+        num_edges = int(num_edges)
         for line in in_file:
             p1, p2, weight = line.split(' ')
             p1 = int(p1)
@@ -54,7 +65,54 @@ if __name__ == '__main__':
             edges[p1][p2] = weight
 
 #    pprint(edges)
-    distance = bellman_ford(edges, 1)
-#    for v in distance:
-#        print "Distance from 1 to %d is %d" % (v, distance[v])
+
+    try:
+        distance = bellman_ford(edges, 1)
+    except AssertionError:
+        print "Abort because negative-cost cycle exists."
+
+    # Create a special-purpose source vertex s, which has edges of length 0 to every vertex
+    edges[0] = {}
+    for v in range(1, num_vertices + 1):
+        edges[0][v] = 0
+
+#    pprint(edges)
+
+    orig_distance = bellman_ford(edges, 0)
+#    pprint(orig_distance)
+
+    # Re-weight each edge to make all edges positive
+    positive_edges = reweight_edges(edges, orig_distance)
+    del positive_edges[0]
+
+    print "After reweight"
+#    pprint(positive_edges)
+
+    # Run Dijkstra for each vertex against new graph and find all pair shortest path
+    # TODO: use itertools.namedtuple
+    # TODO: use Dijkstra
+    shortest_path_len = sys.maxint
+    shortest_path_u = 0
+    shortest_path_v = 0
+
+    total = len(positive_edges.keys())
+    count = 1
+
+    for u in positive_edges.keys():
+        print "%d/%d" % (count, total)
+        distance = bellman_ford(positive_edges, u)
+#        pprint(distance)
+        
+        for v in distance.keys():
+            if (u == v):
+                continue
+            new_shortest_path_len = distance[v] - orig_distance[u] + orig_distance[v]
+#            print "=== (%d, %d, %d, %d, %d, %d, %d)" % (u, v, distance[v], orig_distance[u], orig_distance[v], new_shortest_path_len, shortest_path_len)
+            if new_shortest_path_len < shortest_path_len:
+                shortest_path_len = new_shortest_path_len
+                shortest_path_u = u
+                shortest_path_v = v
+        count += 1
+    
+    print "u=%d v=%d shortest_path_len=%d" % (shortest_path_u, shortest_path_v, shortest_path_len)
 
